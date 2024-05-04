2549c374f9a2086a0bbd4251d1af0c1171ba83a0
CodeMonkey <aathiq.20210466@iit.ac.lk>
2024-05-04 23:45:21 +0530

    sponsor image added

--- client/src/views/Home/components/Sponser.jsx
+++ client/src/views/Home/components/Sponser.jsx
@@ -1,8 +1,14 @@
-
+import Siraj from "../../../assets/Siraj.png"
+import Tambapanni from  "../../../assets/tambapanni.png"
 export const Sponser=()=>{
     return(
         <div className="sponsors-container">
             <h1> Our Sponsors</h1>
+            <div className="sponsors-group">
+                <img src={Tambapanni}/>
+                <img src={Siraj}/>
+
+            </div>
         </div>
     )
 }
