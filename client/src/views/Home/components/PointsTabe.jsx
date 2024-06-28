02d6b0a6d69ad6f8712050efb19199fcf980a7b8
Aathiq Ahamed <aathiq.a@hsenidmobile.com>
2024-06-29 01:17:21 +0530

    nrr added

--- client/src/views/Home/components/PointsTabe.jsx
+++ client/src/views/Home/components/PointsTabe.jsx
@@ -5,6 +5,8 @@ const PointsTable = ({ pointsTableRef, data, title }) => {
         return data.sort((a, b) => {
             if (a.points !== b.points) {
                 return b.points - a.points;
+            } else if (a.NRR !== b.NRR) {
+                return b.NRR - a.NRR;
             } else {
                 return a.team.localeCompare(b.team);
             }
@@ -27,15 +29,15 @@ const PointsTable = ({ pointsTableRef, data, title }) => {
 
     return (
         <div className="points-table-container" ref={pointsTableRef}>
-            <p>{title}</p>
+            {/*<p>{title}</p>*/}
             <h2>SRILANKAN PREMIER LEAGUE</h2>
             <table className="points-table">
                 <thead>
                 <tr>
-                    <th colSpan={6}></th>
+                    <th colSpan={7}></th>
                 </tr>
                 <tr>
-                    <th colSpan={6}>SRILANKAN PREMIER LEAGUE 2024</th>
+                    <th colSpan={7}>{title}</th>
                 </tr>
                 <tr>
                     <th>Pos</th>
@@ -44,6 +46,8 @@ const PointsTable = ({ pointsTableRef, data, title }) => {
                     <th>W</th>
                     <th>L</th>
                     <th>P</th>
+                    <th>NRR</th>
+
                 </tr>
                 </thead>
                 <tbody>
@@ -59,6 +63,7 @@ const PointsTable = ({ pointsTableRef, data, title }) => {
                                 <td>{teamData.win}</td>
                                 <td>{teamData.lost}</td>
                                 <td>{teamData.points}</td>
+                                <td>{teamData.NRR}</td>
                             </tr>
                             {expandedTeam === teamData.team && (
                                 <tr>
