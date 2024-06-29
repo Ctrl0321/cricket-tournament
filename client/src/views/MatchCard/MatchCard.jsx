6610beb9f230142562d52b941d2b3c311c24cacc
CodeMonkey <aathiq.20210466@iit.ac.lk>
2024-06-29 16:48:49 +0530

    win icon added

--- client/src/views/MatchCard/MatchCard.jsx
+++ client/src/views/MatchCard/MatchCard.jsx
@@ -1,9 +1,9 @@
-// import Icon from "../../assets/win.png";
+import Icon from "../../assets/win.png";
 
 export const MatchCard = ({ match }) => {
     return (
         <div className="match-card-container">
-            {/*{match.win === 1 && <img className="win-icon win1" src={Icon} alt="win1" />}*/}
+            {match.win === 1 && <img className="win-icon win1" src={Icon} alt="win1" />}
             <img className="teamLogo" src={match.teamOneImage} alt="teamOne" />
             <div className="score-container">
                 <h4>{match.date}</h4>
@@ -12,7 +12,7 @@ export const MatchCard = ({ match }) => {
                 <h4>Premier League</h4>
                 <p>{match.place}</p>
             </div>
-            {/*{match.win === 2 && <img className="win-icon win2" src={Icon} alt="win2" />}*/}
+            {match.win === 2 && <img className="win-icon win2" src={Icon} alt="win2" />}
             <img className="teamLogo" src={match.teamTwoImage} alt="teamTwo" />
         </div>
     );
